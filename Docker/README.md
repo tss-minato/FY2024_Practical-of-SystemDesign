588c3057149ecf483202ef156eb2dd830192d5e9
Takafumi Minato <takafumi.minato@tss-net.jp>
2025-01-24 11:54:21 +0900

    一式コミット

---
+++ Docker/README.md
@@ -0,0 +1,17 @@
+# Docker環境構築（Raspberry PI）
+
+* インストール
+
+    ``` bash
+    > curl -fsSL https://get.docker.com -o get-docker.sh
+    > sudo sh get-docker.sh
+    ```
+
+* ユーザ権限で実行できるように設定
+
+    ``` bash
+    > sudo gpasswd -a $USER docker
+    > newgrp docker
+    ```
+
+上記実行後、再起動を行う
---
+++ SourceCode/Websocket/Common/Constant/transmission_type.py
@@ -0,0 +1,20 @@
+CONECT = 0x00 
+''' 伝送種別: 接続 '''
+
+STREAMING = 0x01
+''' 伝送種別: ストリーミング '''
+
+CAMERA_CONNECTION_INFO = 0x10
+''' 伝送種別: カメラ接続情報要求 '''
+
+CAMERA_REGISTRATION_INFO = 0x11
+''' 伝送種別: カメラ登録情報要求 '''
+
+CAMERA_REGISTERATION = 0x20
+''' 伝送種別: カメラ登録要求 '''
+
+CHANGE_CAMERA_SETTINGS = 0x21
+''' 伝送種別: カメラ設定変更要求 '''
+
+CAMERA_DELETE = 0x25
+''' 伝送種別: カメラ削除要求 '''
