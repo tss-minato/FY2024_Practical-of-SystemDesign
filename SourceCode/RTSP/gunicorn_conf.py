f88d7cf9f4d2e153db3f8492c3fabfb6457251f0
tss-minato <41051984+tss-minato@users.noreply.github.com>
2025-01-31 13:19:02 +0900

    Create gunicorn_conf.py

---
+++ SourceCode/RTSP/gunicorn_conf.py
@@ -0,0 +1,15 @@
+from multiprocessing import cpu_count
+
+chdir = '~/Repository/Python/RTSP/Client'
+
+# Socket Path
+bind = 'localhost:8000'
+
+# Worker Options
+workers = cpu_count() + 1
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# Logging Options
+loglevel = 'info'
+accesslog = '~/Repository/Python/RTSP/access_log'
+errorlog = '~/Repository/Python/RTSP/error_log
